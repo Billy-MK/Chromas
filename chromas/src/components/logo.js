548c651dbc21b5c3c988e9e4e7d65ee079f252fc
Billy Keating <wmontoothkeating@gmail.com>
2021-08-09 10:57:11 -0700

    Centered and margined logo, made unselectable

--- chromas/src/components/logo.js
+++ chromas/src/components/logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Logo() {
-    return <div className="logo">Chromas.</div>
+    return (
+        <div className="logo-container">
+            <div className="logo">Chromas.</div> 
+        </div>
+    )
 }
 
 export default Logo
