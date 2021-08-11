7b951878f78c8b62ed1525eaddbc52dd73547ad6
Billy Keating <wmontoothkeating@gmail.com>
2021-08-11 15:34:38 -0700

    Started building nav bar (positioning, shape, color)

--- chromas/src/components/Nav/Nav.js
+++ chromas/src/components/Nav/Nav.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Nav() {
     return (
         <nav className="nav-container">
-            
+            <aside className="menu">
+                <ul className="menu-label">
+                    <li><a>Home</a></li>
+                </ul>
+            </aside>
         </nav>
     )
 }
